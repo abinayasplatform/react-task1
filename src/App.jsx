0d36b3d72ad4cd0ec247e92e588c70a8dbfd89eb
abinayasplatform <abinayaraja1624@gmail.com>
2024-03-13 13:51:21 +0530

    guvitask22 --react(price card)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const PriceCard = ({ plan, price, users, storage, publicProjects, access, privateProjects, phoneSupport, subdomain, statusReports,button }) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="price-card">
 
-export default App
+      <h4 className="plan">{plan}</h4>
+      
+      <h1 className="month">${price}/month</h1>
+      
+        <p>✓ {users} Users</p>
+
+        <p >✓ {storage} Storage</p>
+
+        <p className={publicProjects ? 'show' : 'hide'}>
+          {publicProjects ? '✓ Unlimited Public Projects': '✘ Unlimited Public Projects ' } 
+          </p>
+
+        <p className={access ? 'show' : 'hide'}>
+          {access ? '✓ Community Access' : '✘ Community Access'}
+          </p>
+
+        <p className={privateProjects ? 'show' : 'hide'}>
+          {privateProjects ? '✓ Unlimited Private Projects' : " ✘ Unlimited Private Projects"} 
+          </p>
+
+        <p className={phoneSupport ? 'show' : 'hide'}>
+          {phoneSupport ? '✓ Dedicated Phone Support' : ' ✘  Dedicated Phone Support'}
+          </p>
+
+        <p className={subdomain ? 'show' : 'hide'}>
+          {subdomain ? '✓ Free Subdomain' : '✘ Free Subdomain' }
+          </p>
+
+        <p className={statusReports ? 'show' : 'hide'}>
+          {statusReports ? '✓ Monthly Status Reports' : '✘ Monthly Status Reports'}
+          </p>
+
+        <button className={button ? 'enabled' : 'disabled'}>{button ? 'BUTTON' : 'BUTTON'}</button>
+
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <PriceCard
+        plan="FREE"
+        price={0}
+        users="Single"
+        storage="50GB"
+        publicProjects={true}
+        access={true}
+        privateProjects={false}
+        phoneSupport={false}
+        subdomain={false}
+        statusReports={false}
+        button={false}
+      />
+      <PriceCard
+        plan="PLUS"
+        price={9}
+        users={5}
+        storage="50GB"
+        publicProjects={true}
+        access={true}
+        privateProjects={true}
+        phoneSupport={true}
+        subdomain={true}
+        statusReports={false}
+        button={false}
+      />
+      <PriceCard
+        plan="PRO"
+        price={49}
+        users="Unlimited"
+        storage="50GB"
+        publicProjects={true}
+        access={true}
+        privateProjects={true}
+        phoneSupport={true}
+        subdomain={true}
+        statusReports={true}
+        button={true}
+      />
+    </div>
+  );
+};
+
+export default App;
+
